b175552a974a56c46f89c4a0fbbf917a33b2821c
Hem Raj Khanal <hemx0005@elev.tradium.dk>
2020-03-30 14:09:00 +0200

    k

--- Eksamensprojekt (Mads)/Eksamensprojekt (Mads)/Form1.vb
+++ Eksamensprojekt (Mads)/Eksamensprojekt (Mads)/Form1.vb
@@ -1,10 +1,9 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
         Mat.Show()
         Me.Hide()
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
     End Sub
 End Class
