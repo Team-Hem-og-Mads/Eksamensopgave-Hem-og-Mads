5669fb7482d175cc01e016cdaf436a48e8055cae
Hem Raj Khanal <hemx0005@elev.tradium.dk>
2020-03-30 15:37:14 +0200

    hem

--- Eksamensprojekt (Mads)/Eksamensprojekt (Mads)/Form1.Designer.vb
+++ Eksamensprojekt (Mads)/Eksamensprojekt (Mads)/Form1.Designer.vb
@@ -30,7 +30,7 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(329, 110)
+        Me.PictureBox1.Location = New System.Drawing.Point(322, 110)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(203, 208)
         Me.PictureBox1.TabIndex = 0
